0ff94f62924dc3e08cbea2b14ef4b448dce75ae4
OksanaTryhub <ksanatrigub@gmail.com>
2023-01-21 00:21:20 +0200

    added svg icon

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,4 @@
+import SvgIcon from '../SvgIcon/SvgIcon';
 import css from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
@@ -6,7 +7,9 @@ const ContactList = ({ contacts, onDeleteContact }) => (
       <li key={id} className={css.contactList__item}>
         <p>{name}: </p>
         <p>{number}</p>
-        <button onClick={() => onDeleteContact(id)}>Delete</button>
+        <button onClick={() => onDeleteContact(id)}>
+          <SvgIcon id="svg" />
+        </button>
       </li>
     ))}
   </ul>
